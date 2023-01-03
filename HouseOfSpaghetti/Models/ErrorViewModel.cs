37c416b4e839e5dd5c9950aa314e9607382b8cb9
MattColaProgrammer93 <mattchoque115@gmail.com>
2023-01-03 07:36:56 -0800

    Add project files.

---
+++ HouseOfSpaghetti/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseOfSpaghetti.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
